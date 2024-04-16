f2789675682afad1f49070db867f5028f79ecc6f
aujezus <augispay@gmail.com>
2024-04-16 19:59:13 +0300

    Uploadthing added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { Inter } from "next/font/google";
 import TopNav from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { ourFileRouter } from "./api/uploadthing/core";
+import { extractRouterConfig } from "uploadthing/server";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +28,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
           <TopNav />
           {children}
